2371ec8954043ba5ad801cd8806c558872cc3f79
Donald-Edianm <kobbyedinamdonald@gmail.com>
2025-10-24 18:44:33 +0000

    Chore: Improved nav layout

--- src/components/navbar-04/navbar-04.tsx
+++ src/components/navbar-04/navbar-04.tsx
@@ -1,12 +1,37 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Logo } from "./logo";
 import { NavMenu } from "./nav-menu";
 import { NavigationSheet } from "./navigation-sheet";
+import { useEffect, useState } from "react";
 
 const Navbar04Page = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="bg-muted">
-      <nav className="fixed top-6 inset-x-4 h-16 bg-background border dark:border-slate-700/70 max-w-(--breakpoint-xl) mx-auto rounded-full z-100">
+      <nav
+        className={`fixed top-6 inset-x-4 h-16 bg-background/80 border dark:border-slate-700/70 max-w-(--breakpoint-xl) mx-auto rounded-full z-100 shadow-md ${
+          scrolled ? "backdrop-blur-md" : ""
+        }`}
+      >
         <div className="h-full flex items-center justify-between mx-auto px-4">
           <Logo />
 
